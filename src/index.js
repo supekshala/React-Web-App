ae07abbe70786c7db55c1a751d2d010361edb731
supekshala <kavindukv@live.com>
2021-03-22 02:33:31 +0530

    uploading to remote

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from './Components/counter';
+import counter from './Components/counter.jsx';
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+
+
+
+
+reportWebVitals();
